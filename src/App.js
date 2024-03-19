ad2355d5868298b0957446f7644880218a5689e0
lander <guevarralander0@gmail.com>
2024-03-19 14:44:34 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Box, Grid, Heading, Text } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box>
+        <Grid gridTemplateColumns="1fr 1fr">
+          <Grid gridTemplateRows="1fr 1fr 1fr">
+            <Heading>Gio</Heading>
+            <Heading>Alvarez</Heading>
+            <Text>
+              Greetings! I'm Gio Lazaro Alvarez, a passionate and versatile
+              multimedia artist with a distinctive flair for creative
+              expression. My journey in the realm of visual arts encompasses a
+              diverse range of skills, including photography, traditional
+              illustration, digital illustration, and the captivating world of
+              tattoo artistry.
+            </Text>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
   );
 }
 
