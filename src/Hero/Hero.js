61dd4d14e8211c9bff2e7a1a1aee4e292c77d32e
lander <guevarralander0@gmail.com>
2024-03-20 14:45:51 +0800

    fix layout

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -13,64 +13,73 @@ import Asset11 from "../Img/ASSET_11.png";
 function Hero() {
   return (
     <>
-      <Box>
-        <Grid gridTemplateColumns="1fr 1fr" position="relative">
-          <VStack
-            justifyContent="start"
-            alignItems="start"
-            position="absolute"
-            zIndex="2"
-            top="0"
-            left="0"
-            mt="100px"
-            ml="80px"
-          >
-            <Heading
-              fontFamily="Montserrat, sans-serif"
-              fontWeight="bold"
-              fontStyle="italic"
-              fontSize="150px"
-            >
-              Gio
-            </Heading>
-            <Heading
-              fontFamily="Montserrat, sans-serif"
-              fontWeight="bold"
-              fontStyle="italic"
-              fontSize="150px"
-            >
-              Alvarez
-            </Heading>
-            <Text>
-              Greetings! I'm Gio Lazaro Alvarez, a passionate and versatile
-              multimedia artist with a distinctive flair for creative
-              expression. My journey in the realm of visual arts encompasses a
-              diverse range of skills, including photography, traditional
-              illustration, digital illustration, and the captivating world of
-              tattoo artistry.
-            </Text>
-          </VStack>
-          <Box position="relative" zIndex="1" ml="600px">
+      <VStack justifyContent="center" alignItems="center" bgColor="red">
+        <Box bgColor="blue" w="1440px" h="1024px">
+          <Grid gridTemplateColumns="1fr 1fr" position="relative">
             <Box
-              w="666px"
-              h="666px"
-              borderRadius="full"
-              bgColor="#F77E08"
-              mt="200px"
-              zIndex="1"
-            />
-            <Image
-              src={Asset11}
-              w="64%"
               position="absolute"
-              top="0"
-              left="38px"
               zIndex="2"
-              ml="120px"
-            />
-          </Box>
-        </Grid>
-      </Box>
+              top="0"
+              left="0"
+              mt="220px"
+              ml="80px"
+            >
+              <VStack justifyContent="start" alignItems="start">
+                <Heading
+                  fontFamily="Montserrat, sans-serif"
+                  fontWeight="bold"
+                  fontStyle="italic"
+                  fontSize="180px"
+                  letterSpacing="-9.6px"
+                  color="#1E1E1E"
+                >
+                  Gio
+                  <br />
+                  Alvarez
+                </Heading>
+                <Box mt="50px">
+                  <Text fontFamily="Bebas Neue Regular" fontSize="18px">
+                    Greetings! I'm Gio Lazaro Alvarez, a passionate and
+                    versatile multimedia artist
+                    <br />
+                    with a distinctive flair for creative expression. My journey
+                    in the realm of visual arts
+                    <br />
+                    encompasses a diverse range of skills, including
+                    photography, traditional illustration,
+                    <br />
+                    digital illustration, and the captivating world of tattoo
+                    artistry.
+                  </Text>
+                </Box>
+              </VStack>
+            </Box>
+            <Box position="relative" zIndex="1" ml="600px" mt="49px">
+              <Box mt="-15px">
+                <Box
+                  w="666px"
+                  h="666px"
+                  borderRadius="full"
+                  bgColor="#F77E08"
+                  mt="143px"
+                  zIndex="1"
+                />
+              </Box>
+              <Box>
+                <Image
+                  src={Asset11}
+                  w="61%"
+                  position="absolute"
+                  top="0"
+                  left="38px"
+                  zIndex="2"
+                  ml="147px"
+                />
+              </Box>
+            </Box>
+          </Grid>
+        </Box>
+      </VStack>
     </>
   );
 }
